b8407f0579211fde97331da6b2e7dc579c0f3128
LUNA GUEUDET JUAN MANUEL <48184278@ort>
2024-06-19 10:41:37 -0300

    initial commit

---
+++ Assets/controles.cs
@@ -0,0 +1,49 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using UnityEngine;
+
+public class controles : MonoBehaviour
+{
+    public GameObject[] arrayObjetos;
+    private int idx = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            DeactivateAll();
+            arrayObjetos[idx].SetActive(true);
+            if (idx != arrayObjetos.Length - 1)
+            {
+                idx++;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            DeactivateAll();
+            arrayObjetos[idx].SetActive(true);
+            if (idx != 0)
+            {
+                idx--;
+            }
+        }
+    }
+
+    void DeactivateAll()
+    {
+        foreach (var objeto in arrayObjetos)
+        {
+            objeto.SetActive(false);
+        }
+    }
+}
